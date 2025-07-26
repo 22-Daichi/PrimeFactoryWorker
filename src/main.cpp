1477165c555bd7c29d18d041ed4fb3b1d458ca04
22-Daichi <tokyotechdai20@gmail.com>
2025-07-27 00:48:56 +0900

    Co-authored-by: Chikurin1014 <Chikurin1014@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/component/component.hpp>
+
+#include <curses.h>
+
+using namespace std::chrono_literals;
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+    // Create a screen with full width and height fitting the document.
+    auto screen = ftxui::Screen::Create(
+        ftxui::Dimension::Full(), // Width
+        ftxui::Dimension::Full()  // Height
+    );
+
+    float progress1 = 0.0f;
+    float progress2 = 0.0f;
+    while (progress1 < 1.0f || progress2 < 1.0f)
+    {
+        progress1 += 0.01f;
+        progress2 += 0.02f;
+
+        auto document = ftxui::vbox({
+                            ftxui::hbox(
+                                ftxui::gauge(progress1) | ftxui::border | ftxui::flex | ftxui::size(ftxui::WIDTH, ftxui::GREATER_THAN, 50)),
+                            ftxui::hbox(
+                                ftxui::gauge(progress2) | ftxui::border | ftxui::flex |
+                                ftxui::size(ftxui::WIDTH, ftxui::GREATER_THAN, 50)),
+                        }) |
+                        ftxui::center;
+
+        ftxui::Render(screen, document);
+        screen.Print();
+        std::this_thread::sleep_for(10ms);
+        std::cout << screen.ResetPosition(true);
+    }
 }
